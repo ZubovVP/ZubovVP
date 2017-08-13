99840a17b6033cb86c6aa665a7cbd1c6c3a7f3fc
ZubovVP <Zubov.VP@yandex.ru>
2017-08-13 20:11:46 +0300

    task #4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
+
+/**
+* Package for calculate task.
+*
+* @author Vitaly Zubov (mailto:Zubov.VP@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
 	public class Calculate{
-	public static void main (String[] args){
+		public static void main (String[] args){
 		System.out.print("Hello world.");
 }
 }
