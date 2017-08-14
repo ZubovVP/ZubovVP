237d7e720ca039077324946147ec49a68c3d2c30
ZubovVP <Zubov.VP@yandex.ru>
2017-08-14 15:21:56 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,14 +1,18 @@
 package ru.job4j;
 
 /**
-* Package for calculate task.
-*
-* @author Vitaly Zubov (mailto:Zubov.VP@yandex.ru)
-* @version $Id$
-* @since 0.1
-*/
-	public class Calculate{
-		public static void main (String[] args){
-		System.out.print("Hello world.");
-}
+ * Calculate.
+ *
+ * @author Vitaly Zubov (mailto:Zubov.VP@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+		/**
+     * Main.
+     * @param args - args.
+     */
+	public static void main (String[] args){
+		System.out.print("Hello World");
+	}
 }
