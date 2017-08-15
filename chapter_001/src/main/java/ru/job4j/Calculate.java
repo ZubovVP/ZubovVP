d7d47a31a31e80a7b6baf69303100a2223aaca85
ZubovVP <Zubov.VP@yandex.ru>
2017-08-15 16:54:32 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -8,11 +8,11 @@ package ru.job4j;
  * @since 0.1
  */
 public class Calculate {
-		/**
+	/**
      * Main.
      * @param args - args.
      */
-	public static void main (String[] args){
-		System.out.print("Hello World");
+	public static void main(String[] args) {
+		System.out.println("Hello World");
 	}
 }
--- chapter_001/src/test/java/ru/job4j/package-info.java
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -1,8 +1,8 @@
-package ru.job4j;
 /**
 * Package for calculate task.
 *
-* @author Petr Arsentev (mailto:parsentev@yandex.ru)
+* @author Vitaly Zubov (mailto:Zubov.VP@yandex.ru)
 * @version $Id$
 * @since 0.1
 */
+package ru.job4j;
