6221d67c9c46d479caac1a37d9708397ca0d586b
ZubovVP <Zubov.VP@yandex.ru>
2017-08-31 11:09:44 +0300

    3.4. Максимум из трёх чисел[#22937]#2

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -3,10 +3,11 @@ package ru.job4j;
 import java.lang
 
 public class Max {
-    public int max(int first , int second) {
-        return firs > second ? firs : second;
-    }
+	public int max(int first , int second) {
+          return first > second ? first : second;
+      }
 	public int max(int first, int second, int third) {
-		return Math.max(first, second, third)
+		int max = first > second ? first : second;
+		return max > third ? max : third 
 	}
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -8,20 +8,20 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
 	@Test
 	public void whenFirstLessSecond() {
-		Max maxim = new Max();
-		int result = maxim.max(1, 2);
-		assertThat(result, is(2));
-		}
-	public void whenFirstMoreSecond() {
-		Max maxim = new Max();
-		int result = maxim.max(5, 2);
-		assertThat(result, is(5));
-		}
-	public void whenFirstSameSecond() {
-		Max maxim = new Max();
-		int result = maxim.max(3, 3);
-		assertThat(result, is(3));
-		}
+ 		Max maxim = new Max();
+ 		int result = maxim.max(1, 2);
+ 		assertThat(result, is(2));
+ 		}
+ 	public void whenFirstMoreSecond() {
+ 		Max maxim = new Max();
+ 		int result = maxim.max(5, 2);
+ 		assertThat(result, is(5));
+ 		}
+ 	public void whenFirstSameSecond() {
+ 		Max maxim = new Max();
+  		int result = maxim.max(3, 3);
+  		assertThat(result, is(3));
+  		}
 	public void whenFirstMoreSecondMoreThird() {
 		Max maxim = new Max();
 		int result = maxim.max(5, 2, 4);
