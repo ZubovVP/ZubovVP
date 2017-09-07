5d98e61429de0b8688be0040a49cce857333fa4b
ZubovVP <Zubov.VP@yandex.ru>
2017-09-07 11:17:55 +0300

    5.1. Создать программу для сортировки массива методом перестановки. [#195]

---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest{
+	@Test
+	public void whenSortArrayTenElements(){
+		//Задаём произвольный массив(нечётный)
+		int [] numbers = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+		//Отправляем заданный массив в метод
+		 int [] result =numbers.sort();
+		 //Указываем ожидаемый результат
+		 int [] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		 //Сравниваем полученный и ожидаемый результат
+		 assertThat(result, is(expected));
+	
+}
