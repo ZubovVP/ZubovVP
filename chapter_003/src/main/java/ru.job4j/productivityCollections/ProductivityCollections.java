3fccb32357d3bde6a37caad284fd26761702983a
ZubovVP <Zubov.VP@yandex.ru>
2018-02-02 10:19:32 +0300

    Test productivity collections

---
+++ chapter_003/src/main/java/ru.job4j/productivityCollections/ProductivityCollections.java
@@ -0,0 +1,50 @@
+package ru.job4j.productivityCollections;
+
+import java.util.Collection;
+
+/**
+ *This task checks productivity diffrent collections
+ *
+ * @author Vitaly Zubov (mailto:Zubov.VP@yandex.ru).
+ * @version $Id$
+ * @since 01.02.2018
+ */
+public class ProductivityCollections {
+  private long start;
+  private  long finish;
+
+    /**
+     * This method can add values in the collection
+     *
+     * @param collection - the collection
+     * @param amount - amont
+     * @return long - time work
+     */
+    public long add(Collection<String> collection, int amount) {
+        start = System.currentTimeMillis();
+        for(int x = 0; x < amount; x++){
+            collection.add(String.valueOf(x));
+        }
+        finish = System.currentTimeMillis();
+        return finish - start;
+    }
+
+    /**
+     * This method can remove values from the collection
+     *
+     * @param collection - the collection
+     * @param amount - amont
+     * @return long - time work
+     */
+    public long delete(Collection<String> collection, int amount) {
+        for(int y = 0; y < amount + 1000; y++){
+            collection.add(String.valueOf(y));
+        }
+        start = System.currentTimeMillis();
+        for(int x = 0; x < amount; x++ ){
+            collection.remove(String.valueOf(x));
+        }
+        finish = System.currentTimeMillis();
+        return finish - start;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/productivityCollections/ProductivityCollectionsTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.productivityCollections;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+import static org.junit.Assert.*;
+
+public class ProductivityCollectionsTest {
+    @Test
+    public void addTestArrayList() throws Exception {
+        ProductivityCollections test = new ProductivityCollections();
+        ArrayList<String> list = new ArrayList<>();
+        long result = test.add(list, 10000);
+        System.out.printf("%s%d%n", "Add ArrayList - " , result);
+    }
+
+    @Test
+    public void deleteTestArrayList() throws Exception {
+        ProductivityCollections test = new ProductivityCollections();
+        ArrayList<String> list = new ArrayList<>();
+        long result = test.delete(list, 10000);
+        System.out.printf("%s%d%n", "Remove ArrayList - " , result);
+    }
+    @Test
+    public void addTestLinkedList() throws Exception {
+        ProductivityCollections test = new ProductivityCollections();
+        LinkedList<String> list = new LinkedList<>();
+        long result = test.add(list, 10000);
+        System.out.printf("%s%d%n", "Add LinkedList - " , result);
+    }
+    @Test
+    public void deleteTestLinkedList() throws Exception {
+        ProductivityCollections test = new ProductivityCollections();
+        LinkedList<String> list = new LinkedList<>();
+        long result = test.delete(list, 10000);
+        System.out.printf("%s%d%n", "Remove LinkedList - " , result);
+    }
+    @Test
+    public void addTestTreeSet() throws Exception {
+        ProductivityCollections test = new ProductivityCollections();
+        TreeSet<String> list = new TreeSet<>();
+        long result = test.add(list, 10000);
+        System.out.printf("%s%d%n", "Add TreeSet - " , result);
+    }
+    @Test
+    public void deleteTestTreeSet() throws Exception {
+        ProductivityCollections test = new ProductivityCollections();
+        TreeSet<String> list = new TreeSet<>();
+        long result = test.delete(list, 10000);
+        System.out.printf("%s%d%n", "Remove TreeSet - " , result);
+    }
+
+}
