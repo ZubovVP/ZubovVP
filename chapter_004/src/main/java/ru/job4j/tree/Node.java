b343e4d7f9c4b145190423b70796e12334b5813c
ZubovVP <Zubov.VP@yandex.ru>
2018-04-25 15:37:44 +0300

    Add SimpleTree and Node

---
+++ chapter_004/src/main/java/ru/job4j/tree/Node.java
@@ -0,0 +1,131 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+/**
+ * @author Vitaly Zubov (zubovvp@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Node<E extends Comparable<E>> implements SimpleTree<E> {
+    private final List<Node<E>> children = new ArrayList<>();
+    private final E value;
+    private int modCount;
+
+    /**
+     * Constructor
+     *
+     * @param value - value of element.
+     */
+    public Node(final E value) {
+        this.value = value;
+    }
+
+    /**
+     * Add element in the Tree.
+     *
+     * @param parent parent.
+     * @param child  child.
+     * @return - result.
+     */
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> parentNode = findBy(parent);
+        boolean result = false;
+        if (parentNode.isPresent() && !findBy(child).isPresent()) {
+            parentNode.get().children.add(new Node<>(child));
+                result = true;
+                modCount++;
+        }
+        return result;
+    }
+
+
+    /**
+     * Return list of children.
+     *
+     * @return - List.
+     */
+    public List<Node<E>> leaves() {
+        return this.children;
+    }
+
+    /**
+     * Find Node for help value.
+     *
+     * @param value - value.
+     * @return - Node.
+     */
+        public Optional<Node<E>> findBy(E value) {
+            Optional<Node<E>> rsl = Optional.empty();
+            Queue<Node<E>> data = new LinkedList<>();
+            data.offer(this);
+            while (!data.isEmpty()) {
+                Node<E> el = data.poll();
+                if (el.eqValue(value)) {
+                    rsl = Optional.of(el);
+                    break;
+                }
+                for (Node<E> child : el.leaves()) {
+                    data.offer(child);
+                }
+            }
+            return rsl;
+        }
+
+    /**
+     * Compare element with this value.
+     *
+     * @param that - element.
+     * @return - result.
+     */
+    public boolean eqValue(E that) {
+        return this.value.compareTo(that) == 0;
+    }
+
+
+    /**
+     * Iterator.
+     *
+     * @return - iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        int expectCount = modCount;
+        return new Iterator<E>() {
+            int count = 0;
+            List<E> list = checkList();
+            private List<E>  checkList() {
+                Queue<Node<E>> data = new LinkedList<>();
+                List<E> list = new ArrayList<>();
+                data.offer(Node.this);
+                while (!data.isEmpty()) {
+                    Node<E> el = data.poll();
+                    list.add(el.value);
+                    for (Node<E> child : el.leaves()) {
+                        data.offer(child);
+                    }
+                }
+                return list;
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (expectCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < list.size();
+            }
+
+            @Override
+            public E next() {
+                if (expectCount != modCount) {
+                    throw new ConcurrentModificationException();
+                } else if (count == list.size()) {
+                   throw new NoSuchElementException();
+                }
+                return list.get(count++);
+            }
+        };
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/tree/NodeTreeTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Tests
+ *
+ * @author Vitaly Zubov (zubovvp@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class NodeTreeTest {
+
+    @Test
+    public void when6ElFindLastThen6() {
+        Node<Integer> tree = new Node<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Node<Integer> tree = new Node<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenAdd7ElementsAndTestIterator() {
+        Node<Integer> tree = new Node<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        tree.add(1, 7);
+
+        Iterator<Integer> itr = tree.iterator();
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(1));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(2));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(3));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(4));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(7));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(5));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(6));
+        assertFalse(itr.hasNext());
+        itr.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenAddDuplecateElementAndCheckConcurrentModificationException() {
+        Node<Integer> tree = new Node<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 3);
+        tree.add(1, 3);
+
+        Iterator<Integer> itr = tree.iterator();
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(1));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(2));
+        assertTrue(itr.hasNext());
+        assertThat(itr.next(), is(3));
+        assertFalse(itr.hasNext());
+        tree.add(1, 4);
+        itr.next();
+    }
+}
