556abaa7846d704564a27539f43b857301484200
ZubovVP <Zubov.VP@yandex.ru>
2018-08-13 16:38:02 +0300

    Correct EmailNotification.

--- chapter_005/src/main/java/ru/job4j/wait/EmailNotification.java
+++ chapter_005/src/main/java/ru/job4j/wait/EmailNotification.java
@@ -2,12 +2,45 @@ package ru.job4j.wait;
 
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by ZubovVP on 31.07.2018
  * zubovvp@yadndex.ru
  */
 @ThreadSafe
 public class EmailNotification {
+    private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private BlockingQueue<User> users;
+
+    public EmailNotification(BlockingQueue<User> users) {
+        this.users = users;
+    }
+
+    public void start() throws InterruptedException {
+        for (int x = 0; x < this.users.size(); x++) {
+            this.pool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    User us = null;
+                    try {
+                        us = users.poll(1000, TimeUnit.MILLISECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (us != null) {
+                        emailTo(us);
+                    }
+                }
+            });
+        }
+        this.pool.shutdown();
+        this.pool.awaitTermination(1, TimeUnit.DAYS);
+    }
+
 
     /**
      * Send pattern on email.
---
+++ chapter_005/src/test/java/ru/job4j/wait/EmailNotificationTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.wait;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by ZubovVP on 31.07.2018
+ * zubovvp@yadndex.ru
+ */
+public class EmailNotificationTest {
+    BlockingQueue<User> users = new LinkedBlockingDeque<>();
+    //private ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    @Test
+    public void testEmailTo() throws InterruptedException {
+        users.addAll(Arrays.asList(new User("Duke", "duke@yandex.ru"), new User("Kate", "kate@yandex.ru"), new User("Tom", "tom@google.com")));
+        EmailNotification em = new EmailNotification(users);
+        em.start();
+        assertThat(users.size(), is(0));
+    }
+}
