f2502c578d8d895c516b2856d5d2b88ebf6f3a0b
ZubovVP <Zubov.VP@yandex.ru>
2018-12-11 11:09:44 +0300

    Add EchoServlets

---
+++ chapter_007/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,21 @@
+package ru.job4j.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ZubovVP on 28.11.2018
+ * zubovvp@yadndex.ru
+ */
+public class EchoServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
