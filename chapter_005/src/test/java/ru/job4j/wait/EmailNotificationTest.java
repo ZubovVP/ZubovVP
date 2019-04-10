36fe35c921e01aff16760d147ba59887813dd5aa
ZubovVP <Zubov.VP@yandex.ru>
2019-04-10 14:43:20 +0300

    Correct EmailNotificationTest.

--- chapter_005/src/test/java/ru/job4j/wait/EmailNotificationTest.java
+++ chapter_005/src/test/java/ru/job4j/wait/EmailNotificationTest.java
@@ -20,7 +20,7 @@ public class EmailNotificationTest {
         this.users.addAll(Arrays.asList(new User("Duke", "duke@yandex.ru"), new User("Kate", "kate@yandex.ru"), new User("Tom", "tom@google.com")));
         EmailNotification em = new EmailNotification(this.users);
         em.start();
-        Thread.sleep(300);
+        Thread.sleep(3000);
         assertThat(this.users.size(), is(0));
     }
 }
