6802fd6f2044a3357f1e7c79637023ecfa30d5d9
ZubovVP <Zubov.VP@yandex.ru>
2019-07-11 23:10:20 +0300

    Correct checkstyle.

--- chapter_005/src/main/java/ru/job4j/pingpong/PingPong.java
+++ chapter_005/src/main/java/ru/job4j/pingpong/PingPong.java
@@ -1,35 +1,35 @@
-package ru.job4j.pingpong;
-
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-
-/**
- * Ping pong.
- *
- * @author Vitaly Zubov (zubovvp@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
-public class PingPong extends Application {
-    private static final String JOB4J = "Пинг-понг www.job4j.ru";
-
-    @Override
-    public void start(Stage stage) {
-        int limitX = 300;
-        int limitY = 300;
-        Group group = new Group();
-        Rectangle rect = new Rectangle(0, 120, 10, 10);
-        group.getChildren().add(rect);
-        RectangleMove rm = new RectangleMove(rect);
-        Thread th = new Thread(rm);
-        th.start();
-        stage.setScene(new Scene(group, limitX, limitY));
-        stage.setTitle(JOB4J);
-        stage.setResizable(false);
-        stage.show();
-        stage.setOnCloseRequest(event ->  th.interrupt());
-    }
-}
+//package ru.job4j.pingpong;
+//
+//import javafx.application.Application;
+//import javafx.scene.Group;
+//import javafx.scene.Scene;
+//import javafx.scene.shape.Rectangle;
+//import javafx.stage.Stage;
+//
+///**
+// * Ping pong.
+// *
+// * @author Vitaly Zubov (zubovvp@yandex.ru)
+// * @version $Id$
+// * @since 0.1
+// */
+//public class PingPong extends Application {
+//    private static final String JOB4J = "Пинг-понг www.job4j.ru";
+//
+//    @Override
+//    public void start(Stage stage) {
+//        int limitX = 300;
+//        int limitY = 300;
+//        Group group = new Group();
+//        Rectangle rect = new Rectangle(0, 120, 10, 10);
+//        group.getChildren().add(rect);
+//        RectangleMove rm = new RectangleMove(rect);
+//        Thread th = new Thread(rm);
+//        th.start();
+//        stage.setScene(new Scene(group, limitX, limitY));
+//        stage.setTitle(JOB4J);
+//        stage.setResizable(false);
+//        stage.show();
+//        stage.setOnCloseRequest(event ->  th.interrupt());
+//    }
+//}
--- chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
+++ chapter_005/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -1,46 +1,46 @@
-package ru.job4j.pingpong;
-
-import javafx.scene.shape.Rectangle;
-
-/**
- * @author Vitaly Zubov (zubovvp@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
-public class RectangleMove implements Runnable {
-    private final Rectangle rect;
-
-    /**
-     * Constructor.
-     *
-     * @param rect - Rectangle.
-     */
-    public RectangleMove(Rectangle rect) {
-        this.rect = rect;
-    }
-
-    @Override
-    public void run() {
-        int x = 2;
-        int y = -1;
-        while (!Thread.currentThread().isInterrupted()) {
-            if (this.rect.getY() >= 300) {
-                y = -(int) (Math.random() * 10);
-            } else if (this.rect.getY() <= 0) {
-                y = (int) (Math.random() * 10);
-            } else if (this.rect.getX() >= 300) {
-                x = -2;
-            } else if (this.rect.getX() <= 0) {
-                x = 2;
-            }
-
-            this.rect.setX(this.rect.getX() + x);
-            this.rect.setY(this.rect.getY() + y);
-            try {
-                Thread.sleep(35);
-            } catch (InterruptedException e) {
-                break;
-            }
-        }
-    }
-}
+//package ru.job4j.pingpong;
+//
+//import javafx.scene.shape.Rectangle;
+//
+///**
+// * @author Vitaly Zubov (zubovvp@yandex.ru)
+// * @version $Id$
+// * @since 0.1
+// */
+//public class RectangleMove implements Runnable {
+//    private final Rectangle rect;
+//
+//    /**
+//     * Constructor.
+//     *
+//     * @param rect - Rectangle.
+//     */
+//    public RectangleMove(Rectangle rect) {
+//        this.rect = rect;
+//    }
+//
+//    @Override
+//    public void run() {
+//        int x = 2;
+//        int y = -1;
+//        while (!Thread.currentThread().isInterrupted()) {
+//            if (this.rect.getY() >= 300) {
+//                y = -(int) (Math.random() * 10);
+//            } else if (this.rect.getY() <= 0) {
+//                y = (int) (Math.random() * 10);
+//            } else if (this.rect.getX() >= 300) {
+//                x = -2;
+//            } else if (this.rect.getX() <= 0) {
+//                x = 2;
+//            }
+//
+//            this.rect.setX(this.rect.getX() + x);
+//            this.rect.setY(this.rect.getY() + y);
+//            try {
+//                Thread.sleep(35);
+//            } catch (InterruptedException e) {
+//                break;
+//            }
+//        }
+//    }
+//}
