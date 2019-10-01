f7e6d326c819d6c9d55cac6cfdc5784f23629ee9
ZubovVP <Zubov.VP@yandex.ru>
2019-10-01 20:40:00 +0300

    Add Searcher.

---
+++ chapter_009/src/main/java/ru/job4j/search/Searcher.java
@@ -0,0 +1,120 @@
+package ru.job4j.search;
+
+import ru.job4j.io.Search;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 29.09.2019.
+ */
+public class Searcher {
+    private Search search;
+
+    public Searcher() {
+        this.search = new Search();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String directory = null;
+        String nameFile = null;
+        String target = null;
+        for (int x = 0; x < args.length; x++) {
+            if (args[x].equals("-d")) {
+                directory = args[x + 1];
+            } else if (args[x].equals("-n")) {
+                nameFile = args[x + 1];
+                //CHECKSTYLE:OFF
+            } else if (args[x].equals("-m")) {
+                //непонятно чего искать по макс?
+                //CHECKSTYLE:ON
+            } else if (args[x].equals("-f")) {
+                nameFile = args[x + 1];
+            } else if (args[x].equals("-o")) {
+                target = args[x + 1];
+            }
+        }
+        if (directory != null && target != null && nameFile != null) {
+            Searcher search = new Searcher();
+            List<File> result = search.find(directory);
+            result = search.filter(result, nameFile);
+            search.createDirectory(result, target);
+        } else {
+            throw new Exception("Parameters source or target not found");
+        }
+    }
+
+    /**
+     * Find all files.
+     *
+     * @param directory - way of directory
+     * @return - List of files.
+     * @throws Exception - if list is empty.
+     */
+    private List<File> find(String directory) throws Exception {
+        List<File> files = this.search.find(directory);
+        if (files.size() == 0) {
+            throw new Exception("List is empty");
+        }
+        return files;
+    }
+
+    /**
+     * Select the right values.
+     *
+     * @param files - list of files.
+     * @param name  - ext.
+     * @return - list of correct values.
+     */
+    private List<File> filter(List<File> files, String name) {
+        List<File> result = new ArrayList<>();
+        char[] nameChar = name.split("\\.")[0].toCharArray();
+        String ext = name.split("\\.")[1];
+        for (File file : files) {
+            if (file.getName().endsWith(ext)) {
+                char[] nameFile = file.getName().split("\\.")[0].toCharArray();
+                for (int x = 0; x < nameChar.length; x++) {
+                    if (nameChar[x] == '?') {
+                        continue;
+                    }
+                    if (nameChar[x] == '*' || x == nameFile.length - 1) {
+                        result.add(file);
+                        break;
+                    }
+                    if (nameChar[x] != nameFile[x]) {
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Create directory with files.
+     *
+     * @param files  - files.
+     * @param target - target.
+     */
+    private void createDirectory(List<File> files, String target) {
+        try {
+            File source = new File(target);
+            if (!source.exists()) {
+                source.mkdir();
+            }
+            File newFile;
+            for (File file : files) {
+                newFile = new File(String.format("%s%s%s", target, "\\", file.getName()));
+                Files.copy(file.toPath(), newFile.toPath());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_009/src/test/java/ru/job4j/search/SearcherTest.java
@@ -0,0 +1,94 @@
+package ru.job4j.search;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 01.10.2019.
+ */
+public class SearcherTest {
+    private final File directory = new File(String.format("%s%s", System.getProperty("user.dir"), "\\test"));
+    private final File file = new File(String.format("%s%s", System.getProperty("user.dir"), "\\TestSearcher.txt"));
+    private final String source = System.getProperty("user.dir");
+    private final String target = this.directory.getPath();
+
+    @Before
+    public void createFile() throws IOException {
+        this.directory.mkdir();
+        this.file.createNewFile();
+        try (FileWriter writer = new FileWriter(file.getPath())) {
+            writer.write("Test!");
+        }
+    }
+
+    @After
+    public void deleteFile() throws IOException {
+        this.file.delete();
+        this.directory.delete();
+    }
+
+    @Test
+    public void testFullName() throws Exception {
+        String nameFile = "TestSearcher.txt";
+        this.start(this.source, nameFile, this.target);
+        File file = new File(target + "\\" + nameFile);
+        this.check(file);
+        file.delete();
+    }
+
+    @Test
+    public void testWithMask() throws Exception {
+        String nameFile = "TestSea*.txt";
+        this.start(this.source, nameFile, this.target);
+        File file = new File(this.target + "\\" + "TestSearcher.txt");
+        this.check(file);
+        file.delete();
+    }
+
+    @Test
+    public void testWithMask2() throws Exception {
+        String nameFile = "T?s?S?a*.txt";
+        this.start(this.source, nameFile, this.target);
+        File file = new File(target + "\\" + "TestSearcher.txt");
+        this.check(file);
+        file.delete();
+    }
+
+    @Test(expected = Exception.class)
+    public void nullDerectory() throws Exception {
+        String nameFile = "T?s?S?a*.txt";
+        this.start(null, nameFile, this.target);
+    }
+
+    @Test(expected = Exception.class)
+    public void nullSource() throws Exception {
+        String nameFile = "T?s?S?a*.txt";
+        this.start(this.source, nameFile, null);
+    }
+
+    private void start(String source, String nameFile, String target) throws Exception {
+        String[] arguments = new String[]{"java", "-jar", "find.jar", "-d", source, "-n", nameFile, "-o", target};
+        Searcher.main(arguments);
+    }
+
+    private void check(File target) {
+        assertTrue(this.directory.exists());
+        assertTrue(target.exists());
+        try (FileReader fileReader = new FileReader(target);
+             BufferedReader reader = new BufferedReader(fileReader)) {
+            assertThat(reader.readLine(), is("Test!"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
