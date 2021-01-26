07d0e870780297f1289a847c8740078521d4be51
ZubovVP <Zubov.VP@yandex.ru>
2021-01-26 22:57:28 +0300

    Add EchoServer.

---
+++ chapter_009/src/main/java/ru/job4j/socket/EchoServer.java
@@ -0,0 +1,49 @@
+package ru.job4j.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 26.01.2021.
+ */
+public class EchoServer {
+
+
+    /**
+     * For start need in console move C:\Tools\curl-7.74.0_2-win64-mingw\bin> and write curl -i http://localhost:9000/?msg=Hello.
+     * For finish need in console write curl -i http://localhost:9000/?msg=Bye.
+     *
+     * @param args - null.
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9000)) {
+            String str;
+            boolean flag = true;
+            while (flag) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    while (!(str = in.readLine()).isEmpty()) {
+                        if (str.split(" ")[1].endsWith("Bye")) {
+                            flag = false;
+                            break;
+                        }
+                        System.out.println(str);
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                }
+            }
+        }
+    }
+}
