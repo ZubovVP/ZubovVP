c4ee0054222e83ff56367643d3accea8b613c2d8
ZubovVP <Zubov.VP@yandex.ru>
2021-02-08 12:26:24 +0300

    Add UserCache.

---
+++ chapter_005/src/main/java/ru/job4j/concurrent/User.java
@@ -0,0 +1,36 @@
+package ru.job4j.concurrent;
+
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 08.02.2021.
+ */
+public class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/concurrent/UserCache.java
@@ -0,0 +1,38 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Intellij IDEA.
+ * User: Vitaly Zubov.
+ * Email: Zubov.VP@yandex.ru.
+ * Version: $Id$.
+ * Date: 08.02.2021.
+ */
+@ThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> result = new ArrayList<>();
+        for (User user : this.users.values()) {
+            result.add(User.of(user.getName()));
+        }
+        return result;
+    }
+}
